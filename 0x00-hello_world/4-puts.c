f385e995926a766cad662ed42b47d7d0ce534854
emmanuelakbi <emmanuelakinfulubi@gmail.com>
2023-02-16 15:42:38 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * description: C program that prints, using the function puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
