6dea94fe775031e45081a67fd9a7028aea541415
emmanuelakbi <emmanuelakinfulubi@gmail.com>
2023-02-16 16:59:42 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * Main: print quote
+ * Main - print quote
  *
- * Description: prints dora quote
+ * Description: prints Dora's quote
  *
  * Return: Always 1 (Success)
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
