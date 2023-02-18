3793395a34a91a77009eb9478e1ea9a580968182
emmanuelakbi <emmanuelakinfulubi@gmail.com>
2023-02-18 10:51:31 -0800

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -20,7 +20,7 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	} else
-       	{
+	{
 		printf("%d is zero\n", n);
 	}
 	return (0);
