3a4417f69a2a00923d5ba84618b6d669f35a67dc
emmanuelakbi <emmanuelakinfulubi@gmail.com>
2023-02-21 06:38:08 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - program that prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char text[10] = "_putchar";
+	int i = 0;
+	
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}	
+	_putchar('\n');
+	return (0);
+}
