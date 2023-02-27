1386315528b381bae90f4c9c21c8cda829bb89e0
emmanuelakbi <emmanuelakinfulubi@gmail.com>
2023-02-27 13:26:08 -0800

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,12 +11,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
-
-	while (src[index])
+	int i;
+	
+	for (i = 0; *(src + i) != '\0'; i++)
 	{
-		dest[index] = src[index];
-		index++;
+		dest[i] = *(src + i);
 	}
+	dest[i] = '\0';
+	
 	return (dest);
 }
